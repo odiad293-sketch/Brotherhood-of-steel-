f3c2c4f411c377c00ff0a2eb701620ac7fa58a2e
etiosa <odiad293@gmail.com>
2026-09-08 22:24:05 +0100

    update test-pnw.js

--- BHOS/api/test-pnw.js
+++ BHOS/api/test-pnw.js
@@ -1,9 +1,33 @@
 export default async function handler(req, res) {
+  const apiKey = process.env.PNW_API_KEY;
 
-    const apiKey = process.env.PNW_API_KEY;
+  const query = `
+    query {
+      nations(first: 1) {
+        data {
+          nation_name
+        }
+      }
+    }
+  `;
 
-    // send GraphQL request using apiKey
+  try {
+    const response = await fetch("https://api.politicsandwar.com/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${apiKey}`
+      },
+      body: JSON.stringify({ query })
+    });
 
-    // return P&W data
-    console.log('gfhgjhj')
+    const data = await response.json();
+
+    res.status(200).json(data);
+
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
+  }
 }
